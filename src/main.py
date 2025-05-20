b6be5b14a82addbc62d7aaad7dbecd3f1700cc00
frostyOtter <binary.hamlet147@gmail.com>
2025-05-20 22:22:21 +0700

    refactor: quick start, free resources on embeddings service select, support input model configs

--- src/main.py
+++ src/main.py
@@ -59,20 +59,11 @@ def perform_word_analogy(
         return None, None
 
 
-def get_embedding_model() -> Callable:
-    """Returns the selected embedding model based on user choice."""
-    from dotenv import load_dotenv
-
-    load_dotenv(override=True)
-    import os
-
-    embedding_service = st.selectbox(
-        "Select Embedding Service",
-        ["GloVe", "Cohere", "OpenAI", "Azure_OpenAI", "SBERT"],
-        index=1,
-        key="embedding_service",
-    )
-
+def get_embedding_model(embedding_service: str, service_configs: dict) -> Callable:
+    """Returns the selected embedding model based on user choice.
+    embedding_service: str - The embedding service to use.
+    Supports: GloVe, Cohere, OpenAI, Azure_OpenAI, SBERT
+    """
     try:
         if embedding_service == "GloVe":
             from glove_embeddings import GloveEmbeddings
@@ -81,46 +72,33 @@ def get_embedding_model() -> Callable:
         elif embedding_service == "Cohere":
             from cohere_embeddings import CohereEmbeddings
 
-            api_key = os.getenv("COHERE_API_KEY")
-            if api_key is None:
-                st.error("Beep Boop, Cohere API key is not set.")
-                return None
-            return CohereEmbeddings(api_key=api_key)
+            return CohereEmbeddings(
+                api_key=service_configs["api_key"],
+                model_name=service_configs["model_name"],
+            )
+
         elif embedding_service == "OpenAI":
             from openai_embeddings import OpenAIEmbeddings
 
-            model_name = os.getenv("OPENAI_MODEL")
-            api_key = os.getenv("OPENAI_API_KEY")
-            if any(model_name, api_key) is None:
-                st.error("Beep Boop, OpenAI model or API key is not set.")
-                return None
             return OpenAIEmbeddings(
-                api_key=os.getenv("OPENAI_API_KEY"), model_name=model_name
+                api_key=service_configs["api_key"],
+                model_name=service_configs["model_name"],
             )
+
         elif embedding_service == "Azure_OpenAI":
             from azure_openai_embeddings import AzureOpenAIEmbeddings
 
-            api_key = os.getenv("AZURE_OPENAI_API_KEY")
-            endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-            model_name = os.getenv("AZURE_OPENAI_DEPLOYMENT")
-            if any(api_key, endpoint, model_name) is None:
-                st.error("Beep Boop, Azure OpenAI model or API key is not set.")
-                return None
             return AzureOpenAIEmbeddings(
-                api_key=api_key,
-                endpoint=endpoint,
-                model_name=model_name,
+                api_key=service_configs["api_key"],
+                endpoint=service_configs["endpoint"],
+                model_name=service_configs["model_name"],
             )
+
         elif embedding_service == "SBERT":
             from sbert_embeddings import SBertEmbeddings
 
-            model_name = st.text_input(
-                label="Enter model name", value="all-MiniLM-L6-v2"
-            )
-            if model_name is None:
-                st.error("Beep Boop, model name is not set.")
-                return None
-            return SBertEmbeddings(model_name=model_name)
+            return SBertEmbeddings(model_name=service_configs["model_name"])
+
     except Exception as e:
         st.error(f"Error loading {embedding_service} model: {str(e)}")
         return None
@@ -215,96 +193,158 @@ def add_new_data_to_db(
     return result
 
 
+def on_change_embedding_service():
+    st.session_state["service_configs"] = None
+    st.session_state["embedding_service"] = None
+
+
 def main() -> None:
     st.title("Word Analogy Calculator")
 
-    st.subheader("Enter words with operations (- or +) between them")
-    st.caption("Example: king - man + woman")
-    st.caption("Press Enter when done.")
-
-    embeddings_model = get_embedding_model()
-    if embeddings_model is None:
-        return
-    embedding_service = st.session_state.get("embedding_service", "GloVe")
-    if embedding_service != "GloVe":
-        # Initialize LanceDB on first call
-        embeddings_db = EmbeddingsDB()
-        embeddings_db.connect()
-        if (
-            embedding_service.lower() + "_embeddings"
-            not in embeddings_db.db.table_names()
-        ):
-            st.warning(
-                f"Beep Boop, table '{embedding_service.lower()}_embeddings' does not exist. Please ingest data first."
-            )
-            table_row_count = 0
-        else:
-            table_row_count = embeddings_db.retrieve_table_row_count(
-                embedding_service.lower() + "_embeddings"
-            )
-            if table_row_count == 0:
-                st.warning(
-                    "Beep Boop, No words found in the database. Please ingest data first."
-                )
-            else:
-                st.write(f"Beep Boop, Found {table_row_count} words in the database.")
-        add_data_result = False
-        with st.expander("Ingest more words into database"):
-            # Display a form, user can input number of words to embeddings into database.
-            with st.form("Ingest words into database:"):
-                num_words = st.number_input(
-                    label="Number of words to ingest", min_value=1, max_value=1000
-                )
-                submit_button = st.form_submit_button("Submit")
-                if submit_button:
-                    st.write(
-                        f"Beep Boop, Ingesting {num_words} words into the database, please wait...."
-                    )
-                    add_data_result = add_new_data_to_db(
-                        table_row_count,
-                        embeddings_db,
-                        embeddings_model,
-                        embedding_service,
-                        num_words,
-                    )
-        if add_data_result:
-            st.success("Beep Boop, Ingestion successful.")
-            embeddings_db = None
-            st.rerun()
-
-    with st.form("Input words and operations", enter_to_submit=True):
-        analogy_input = st.text_input(label="Input words and operations", value="")
-        submit_button = st.form_submit_button("Submit")
-
-    if submit_button and analogy_input.strip():
-        # Preprocess user input message into words and operations
-        words, operations = preprocess_analogy_string(analogy_input)
-        if words is None or operations is None:
-            return
-
-        # Perform word analogy calculation
-        target_word_embedding, _ = perform_word_analogy(
-            embeddings_model, words, operations
+    ## User select service and input service configs
+    with st.container():
+        embedding_service = st.selectbox(
+            "Select embedding service",
+            ["GloVe", "Cohere", "OpenAI", "Azure_OpenAI", "SBERT"],
+            index=1,
+            on_change=on_change_embedding_service,
         )
 
         if embedding_service == "GloVe":
-            similar_words = embeddings_model.find_closest_word(
-                target_word_embedding, words
-            )
-            if similar_words:
-                display_similar_words(similar_words)
-            else:
-                st.warning("Beep Boop, GloVe can't find any similar words.")
+            service_configs = {
+                "model_name": st.text_input(
+                    "Enter GloVe model name", "glove-wiki-gigaword-50"
+                )
+            }
+        elif embedding_service == "Cohere":
+            service_configs = {
+                "api_key": st.text_input("Enter Cohere API key", type="password"),
+                "model_name": st.text_input(
+                    "Enter Cohere model name", "embed-english-v3.0"
+                ),
+            }
+        elif embedding_service == "OpenAI":
+            service_configs = {
+                "api_key": st.text_input("Enter OpenAI API key", type="password"),
+                "model_name": st.text_input(
+                    "Enter OpenAI model name", "text-embedding-3-large"
+                ),
+            }
+        elif embedding_service == "Azure_OpenAI":
+            service_configs = {
+                "api_key": st.text_input("Enter Azure OpenAI API key", type="password"),
+                "model_name": st.text_input(
+                    "Enter Azure OpenAI model name", "text-embedding-3-large"
+                ),
+                "endpoint": st.text_input("Enter Azure OpenAI endpoint"),
+            }
         elif embedding_service == "SBERT":
-            st.error("Beep Boop, SBERT embeddings are not implemented yet.")
-        else:  # Cohere, OpenAI, or Azure OpenAI
-            similar_words = get_similar_words_from_db(
-                embeddings_db, embedding_service, target_word_embedding, words
+            service_configs = {
+                "model_name": st.text_input(
+                    "Enter SBERT model name", "all-MiniLM-L6-v2"
+                )
+            }
+
+        submit_button = st.button("Submit")
+        if submit_button:
+            st.session_state["service_configs"] = service_configs
+            st.session_state["embedding_service"] = embedding_service
+            st.rerun()
+
+    ## After submit button is clicked, load the service configs and embedding service
+    service_configs = st.session_state.get("service_configs")
+    embedding_service = st.session_state.get("embedding_service")
+    if service_configs and embedding_service:
+        embeddings_model = get_embedding_model(embedding_service, service_configs)
+
+        ## Checking if there are any data for the selected embedding service in the database
+        if embedding_service != "GloVe":
+            # Initialize LanceDB on first call
+            embeddings_db = EmbeddingsDB()
+            embeddings_db.connect()
+            table_row_count = 0
+            table_name = (
+                embedding_service.lower().strip().replace(" ", "_") + "_embeddings"
             )
-            if similar_words:
-                display_similar_words(similar_words)
+            if table_name not in embeddings_db.db.table_names():
+                st.warning(
+                    f"Beep Boop, table '{table_name}' does not exist. Please ingest data first."
+                )
+
             else:
-                st.warning("Could not find any suitable answers in the vocabulary.")
+                table_row_count = embeddings_db.retrieve_table_row_count(table_name)
+                if table_row_count == 0:
+                    st.warning(
+                        "Beep Boop, Nothing found in the database. Please ingest data first."
+                    )
+                else:
+                    st.write(
+                        f"Beep Boop, Found {table_row_count} words in the database."
+                    )
+
+            ## User can ingest more data into the database
+            add_data_result = False
+            with st.expander("Ingest more words into database"):
+                # Display a form, user can input number of words to embeddings into database.
+                with st.form("Ingest words into database:"):
+                    num_words = st.number_input(
+                        label="Number of words to ingest", min_value=1, max_value=1000
+                    )
+                    submit_button = st.form_submit_button("Submit")
+                    if submit_button:
+                        st.write(
+                            f"Beep Boop, Ingesting {num_words} words into the database, please wait...."
+                        )
+                        add_data_result = add_new_data_to_db(
+                            table_row_count,
+                            embeddings_db,
+                            embeddings_model,
+                            embedding_service,
+                            num_words,
+                        )
+            if add_data_result:
+                st.success("Beep Boop, Ingestion successful.")
+                st.rerun()
+
+        st.subheader("Enter words with operations (- or +) between them")
+        st.caption("Example: king - man + woman")
+        st.caption("Press Enter when done.")
+
+        with st.form("Input words and operations", enter_to_submit=True):
+            analogy_input = st.text_input(label="Input words and operations", value="")
+            submit_button = st.form_submit_button("Submit")
+
+            if submit_button and analogy_input.strip():
+                # Preprocess user input message into words and operations
+                words, operations = preprocess_analogy_string(analogy_input)
+                if words is None or operations is None:
+                    return
+
+                # Perform word analogy calculation
+                target_word_embedding, _ = perform_word_analogy(
+                    embeddings_model, words, operations
+                )
+
+                if embedding_service == "GloVe":
+                    similar_words = embeddings_model.find_closest_word(
+                        target_word_embedding, words
+                    )
+                    if similar_words:
+                        display_similar_words(similar_words)
+                    else:
+                        st.warning("Beep Boop, GloVe can't find any similar words.")
+                elif embedding_service == "SBERT":
+                    st.error("Beep Boop, SBERT embeddings are not implemented yet.")
+                else:  # Cohere, OpenAI, or Azure OpenAI
+                    similar_words = get_similar_words_from_db(
+                        embeddings_db, embedding_service, target_word_embedding, words
+                    )
+                    if similar_words:
+                        display_similar_words(similar_words)
+                    else:
+                        st.warning(
+                            "Could not find any suitable answers in the vocabulary."
+                        )
 
 
 if __name__ == "__main__":
